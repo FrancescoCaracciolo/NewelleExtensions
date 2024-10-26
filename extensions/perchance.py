7a4682428c16176315af7158bee7916bb1193de3
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2024-10-26 18:09:51 +0200

    Use extensions settings

--- extensions/perchance.py
+++ extensions/perchance.py
@@ -7,24 +7,34 @@ import json
 class PerchanceExtension(NewelleExtension):
     name = "Perchance Image Generator"
     id = "perchanceimg"
-    positive_prompt = ""
-    negative_prompt = ""
     def __init__(self, a, b, c) -> None:
         super().__init__(a, b, c) 
-        self.cachefile = os.path.join(self.extension_path, "perchanceimg.json")
-        if not os.path.isdir(self.extension_path):
-            os.makedirs(self.extension_path)
+        self.extension_cache = self.get_setting("cache")
+        if self.extension_cache is None:
+            self.extension_cache = {}
+   
+    def get_extra_settings(self):
+        return [
+            {
+                "key": "positive-prompt",
+                "title": "Positive Prompt",
+                "description": "Positive Prompt to add to the request, you can specify with {prompt} the prompt given by the LLM",
+                "type": "entry",
+                "default": ""
+            },
+            {
+                "key": "negative-prompt",
+                "title": "Negative Prompt",
+                "description": "Things you don't want in the image",
+                "type": "entry",
+                "default": ""
+            }
+        ]
 
-        if not os.path.exists(self.cachefile):
-            with open(self.cachefile, "w+") as f:
-                f.write("{}")
-        
-        self.extension_cache = json.load(open(self.cachefile, "r")) 
+    @staticmethod
+    def requires_sanbox_escape() -> bool:
+        return True
 
-    def save_cache(self):
-        with open(self.cachefile, "w") as f:
-            json.dump(self.extension_cache, f)
-    
     def install(self):
         bash_script = """
         mkdir perchanceapi
@@ -39,9 +49,9 @@ class PerchanceExtension(NewelleExtension):
         old_cwd = os.getcwd()
         os.chdir(self.extension_path)
         if not os.path.exists("perchanceapi"):
-            subprocess.Popen(["flatpak-spawn", "--host", "bash", "-c", bash_script])
-        
+            subprocess.Popen(["flatpak-spawn", "--host", "bash", "-c", bash_script])     
         os.chdir(old_cwd) 
+
     def get_replace_codeblocks_langs(self) -> list:
         return ["generate-image"]
 
@@ -86,7 +96,9 @@ class PerchanceExtension(NewelleExtension):
         # Generate the image and write it to the pixbuf loader
         old_cwd = os.getcwd()
         os.chdir(self.extension_path)
-        codeblock = self.positive_prompt.format(prompt=codeblock)
+        positive_prompt = self.get_setting("positive-prompt")
+        negative_prompt = self.get_setting("negative-prompt")
+        codeblock = positive_prompt.format(prompt=codeblock) if "{prompt}" in positive_prompt else codeblock + ", " + positive_prompt
         if codeblock in self.extension_cache:
             with open("perchanceapi/perchance-image-generator/generated-pictures/" + self.extension_cache[codeblock], 'rb') as response:
                 data = response.read()
@@ -99,14 +111,14 @@ class PerchanceExtension(NewelleExtension):
             bash_script = f"""
 cd perchanceapi/perchance-image-generator
 source venv/bin/activate
-python main.py -n 1 -f {fname} -p {quote_string(codeblock)} -np {quote_string(self.negative_prompt)} -r 512x512"""
+python main.py -n 1 -f {fname} -p {quote_string(codeblock)} -np {quote_string(negative_prompt)} -r 512x512"""
             subprocess.check_output(["flatpak-spawn", "--host", "bash", "-c", bash_script])
             with open("perchanceapi/perchance-image-generator/generated-pictures/" + fname + "1.jpeg", 'rb') as response:
                 data = response.read()
                 pixbuf_loader.write(data)
                 pixbuf_loader.close()
             self.extension_cache[codeblock] = fname + "1.jpeg"
-            self.save_cache()
+            self.set_setting("cache", self.extension_cache)
         except Exception as e:
             print("Exception generating the image: " + str(e))
         os.chdir(old_cwd)
